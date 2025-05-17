7492ab71113031b5528495b2d41da9bb4a17ebeb
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-05-17 15:31:45 +0900

    feat: facemap 툴팁 UI 구현

--- src/components/resultPage/diagnosisSection/diagnosisSection.style.ts
+++ src/components/resultPage/diagnosisSection/diagnosisSection.style.ts
@@ -4,7 +4,7 @@ export const Container = styled.section`
   width: 100%;
   display: grid;
   grid-template-rows: auto auto auto;
-  padding: 48px 0 0 0;
+  padding: 48px 48px 0 48px;
   gap: 56px;
 `;
 
@@ -35,33 +35,36 @@ export const Hotspot = styled.div<{ color?: string }>`
   cursor: pointer;
   background: ${({ color }) => color || 'rgba(241, 138, 138, 0.7)'};
   border-radius: 100px;
+  transform: translateX(-50%);
 `;
 
 export const Tooltip = styled.div<{ visible: boolean; top: number; left: number }>`
   position: fixed;
   pointer-events: none;
-  background: #b3baf3;
-  padding: 6px 10px;
-  border-radius: 8px;
-  border: 1px solid #888;
-  font-size: 14px;
-  color: #000;
+  background: #a7b2e6;
+  padding: 12px 16px;
+  gap: 5px;
+  border-radius: 10px;
+  border: none;
+  font-size: 18px;
+  font-weight: bold;
+  color: black;
   visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
   top: ${({ top }) => top}px;
   left: ${({ left }) => left}px;
-  transform: translateY(-50%);
+  transform: translateY(-59px) translateX(70px);
+  display: flex;
+  flex-direction: column;
+
   white-space: nowrap;
   z-index: 999;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
 
-  &::before {
-    content: '';
-    position: absolute;
-    right: 100%;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 16px;
-    height: 1px;
-    background: black;
-  }
+export const TooltipTitle = styled.p`
+  font-weight: 700;
+  font-size: 22px;
+`;
+export const TooltipLabel = styled.p`
+  font-weight: 500;
 `;
--- src/pages/resultPage/resultPage.style.ts
+++ src/pages/resultPage/resultPage.style.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: auto minmax(720px, 1280px) auto;
+  grid-template-columns: auto minmax(720px, 1080px) auto;
   width: 100%;
 `;
