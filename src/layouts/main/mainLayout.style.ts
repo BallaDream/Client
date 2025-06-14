2c54564cee00beab4a07e66423f0d5707f47b2c5
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-06-15 03:28:24 +0900

    feat: 헤더 레이아웃으로 보냄

--- src/layouts/main/mainLayout.style.ts
+++ src/layouts/main/mainLayout.style.ts
@@ -4,6 +4,7 @@ const Container = styled.div`
   height: 100%;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   width: 100vw;
 `;
 
--- src/layouts/main/mainLayout.tsx
+++ src/layouts/main/mainLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/components/common/header/header';
+
 import * as S from '@/layouts/main/mainLayout.style';
 
 export default function MainLayout() {
   return (
     <S.Container>
+      <Header />
       <Outlet />
     </S.Container>
   );
