fb4884008a65941f8eed41c8d04bebdf88763e17
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-06-15 04:07:42 +0900

    feat: 살짝 반응형

--- src/pages/uploadPage/uploadPage.style.ts
+++ src/pages/uploadPage/uploadPage.style.ts
@@ -2,11 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
   gap: 2em 1em;
-  padding: 8em 0px 0px 0px;
+  padding: 8em 1.5em 0 1.5em;
 `;
