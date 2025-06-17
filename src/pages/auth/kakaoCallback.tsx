42a8382d02c2f0e29f8df97db09c58de7b4495e1
dykimruby <130230167+dykimruby@users.noreply.github.com>
2025-06-17 21:39:59 +0900

    fix: 로그인후 새로고침 시 잘못된 리턴 방지

--- src/pages/auth/kakaoCallback.tsx
+++ src/pages/auth/kakaoCallback.tsx
@@ -16,7 +16,7 @@ export default function KakaoCallback() {
 
   useEffect(() => {
     const fetchKakaoToken = async () => {
-      if (isCalled) return;
+      if (isCalled || !code) return;
       isCalled = true;
 
       try {
@@ -27,19 +27,19 @@ export default function KakaoCallback() {
           dispatch(setAccessToken(token));
           const decoded: any = jwtDecode(token);
           dispatch(setNickname(decoded.nickname));
-          alert('로그인 성공!');
+          alert('카카오 로그인에 성공했습니다!');
           navigate('/');
         } else {
           alert('AccessToken이 응답 헤더에 없습니다.');
         }
       } catch (error: any) {
         console.error('❌ 카카오 로그인 실패:', error);
-        alert('카카오 로그인 중 오류가 발생했습니다.');
+        alert('카카오 로그인 중 문제가 발생했습니다. 다시 시도해주세요.');
       }
     };
 
     fetchKakaoToken();
-  }, [code]);
+  }, [code, dispatch, navigate]);
 
   return <div>카카오 로그인 처리 중입니다...</div>;
 }
