82d75211ac489957177c7a0af945759349f03927
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-07-10 00:19:50 +0900

    fix: queryClient 수정

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -4,6 +4,8 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
     },
   },
 });
